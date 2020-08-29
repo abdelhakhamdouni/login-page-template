0fba913f9b0139022a2c60ffb2b201f74e2688b6
abdelhak hamdouni <abdelhakhamdouni@sfr.fr>
2020-08-29 17:06:27 +0200

    change eye en eye-slash pour que sa correspond

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@ let password_eye = document.querySelector('#password-icone')
 
 password_eye.addEventListener('click', ()=>{
     console.log('ok')
-    if(password_eye.classList.contains('fa-eye-slash')){
-        password_eye.classList.remove('fa-eye-slash')
-        password_eye.classList.add('fa-eye')
+    if(password_eye.classList.contains('fa-eye')){
+        password_eye.classList.remove('fa-eye')
+        password_eye.classList.add('fa-eye-slash')
         document.querySelector('#user_password').setAttribute('type', 'text')
     }
     else{
-        password_eye.classList.remove('fa-eye')
-        password_eye.classList.add('fa-eye-slash')
+        password_eye.classList.remove('fa-eye-slash')
+        password_eye.classList.add('fa-eye')
         document.querySelector('#user_password').setAttribute('type', 'password')
 
     }
